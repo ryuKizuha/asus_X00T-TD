baf3664e3495ce6999dabfd3fd2e0f42150c00d6
root <zulfiray7@gmail.com>
2021-02-26 14:33:56 +0000

    kernel

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -458,15 +458,7 @@ out_release:
 
 int cpu_down(unsigned int cpu)
 {
-	struct cpumask newmask;
 	int err;
-
-	cpumask_andnot(&newmask, cpu_online_mask, cpumask_of(cpu));
-
-	/* One big cluster CPU and one little cluster CPU must remain online */
-	if (!cpumask_intersects(&newmask, cpu_perf_mask) ||
-		!cpumask_intersects(&newmask, cpu_lp_mask))
-		return -EINVAL;
 
 	cpu_maps_update_begin();
 
@@ -564,9 +556,8 @@ out_notify:
 		__cpu_notify(CPU_UP_CANCELED | mod, hcpu, nr_calls, NULL);
 out:
 	cpu_hotplug_done();
-	arch_smt_update();
 	trace_sched_cpu_hotplug(cpu, ret, 1);
-
+	arch_smt_update();
 	return ret;
 }
 
@@ -650,7 +641,6 @@ int disable_nonboot_cpus(void)
 	int cpu, first_cpu, error = 0;
 
 	cpu_maps_update_begin();
-	unaffine_perf_irqs();
 	first_cpu = cpumask_first(cpu_online_mask);
 	/*
 	 * We take down all of the non-boot CPUs in one shot to avoid races
@@ -717,7 +707,7 @@ void enable_nonboot_cpus(void)
 		error = _cpu_up(cpu, 1);
 		trace_suspend_resume(TPS("CPU_ON"), cpu, false);
 		if (!error) {
-			pr_debug("CPU%d is up\n", cpu);
+			pr_info("CPU%d is up\n", cpu);
 			cpu_device = get_cpu_device(cpu);
 			if (!cpu_device)
 				pr_err("%s: failed to get cpu%d device\n",
@@ -732,7 +722,6 @@ void enable_nonboot_cpus(void)
 	arch_enable_nonboot_cpus_end();
 
 	cpumask_clear(frozen_cpus);
-	reaffine_perf_irqs();
 out:
 	cpu_maps_update_done();
 }
@@ -868,35 +857,6 @@ static DECLARE_BITMAP(cpu_isolated_bits, CONFIG_NR_CPUS) __read_mostly;
 const struct cpumask *const cpu_isolated_mask = to_cpumask(cpu_isolated_bits);
 EXPORT_SYMBOL(cpu_isolated_mask);
 
-#if CONFIG_LITTLE_CPU_MASK
-static const unsigned long lp_cpu_bits = CONFIG_LITTLE_CPU_MASK;
-const struct cpumask *const cpu_lp_mask = to_cpumask(&lp_cpu_bits);
-#else
-const struct cpumask *const cpu_lp_mask = cpu_possible_mask;
-#endif
-EXPORT_SYMBOL(cpu_lp_mask);
-
-#if CONFIG_BIG_CPU_MASK
-static const unsigned long perf_cpu_bits = CONFIG_BIG_CPU_MASK;
-const struct cpumask *const cpu_perf_mask = to_cpumask(&perf_cpu_bits);
-#else
-const struct cpumask *const cpu_perf_mask = cpu_possible_mask;
-#endif
-/*
- * This assumes that half of the CPUs are little and that they have lower
- * CPU numbers than the big CPUs (e.g., on an 8-core system, CPUs 0-3 would be
- * little and CPUs 4-7 would be big).
- */
-#define LITTLE_CPU_MASK	((1UL << (NR_CPUS / 2)) - 1)
-#define BIG_CPU_MASK	(((1UL << NR_CPUS) - 1) & ~LITTLE_CPU_MASK)
-static const unsigned long little_cluster_cpus = LITTLE_CPU_MASK;
-const struct cpumask *const cpu_lp_mask = to_cpumask(&little_cluster_cpus);
-EXPORT_SYMBOL(cpu_lp_mask);
-
-static const unsigned long big_cluster_cpus = BIG_CPU_MASK;
-const struct cpumask *const cpu_perf_mask = to_cpumask(&big_cluster_cpus);
-EXPORT_SYMBOL(cpu_perf_mask);
-
 void set_cpu_possible(unsigned int cpu, bool possible)
 {
 	if (possible)
@@ -954,6 +914,11 @@ void init_cpu_online(const struct cpumask *src)
 	cpumask_copy(to_cpumask(cpu_online_bits), src);
 }
 
+void init_cpu_isolated(const struct cpumask *src)
+{
+	cpumask_copy(to_cpumask(cpu_isolated_bits), src);
+}
+
 enum cpu_mitigations cpu_mitigations = CPU_MITIGATIONS_AUTO;
 
 static int __init mitigations_parse_cmdline(char *arg)
@@ -969,11 +934,6 @@ static int __init mitigations_parse_cmdline(char *arg)
 	return 0;
 }
 early_param("mitigations", mitigations_parse_cmdline);
-
-void init_cpu_isolated(const struct cpumask *src)
-{
-	cpumask_copy(to_cpumask(cpu_isolated_bits), src);
-}
 
 static ATOMIC_NOTIFIER_HEAD(idle_notifier);
 
